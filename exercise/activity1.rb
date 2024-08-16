31749010ec1a9508657315a09ff28f72569059f3
Aeron Jonh <ajandres1103@gmail.com>
2024-08-16 15:42:27 +0800

    answer is not in array

---
+++ exercise/activity1.rb
@@ -0,0 +1,20 @@
+# TODO: ACTIVITY 1
+#  Write a function that takes an Array of ages,
+#  the function will return the average age of adults.
+#  Print on console the result.
+
+
+# answer is not in array
+
+def calculate_bmi(age1, age2, age3, age4, age5)
+
+  add_age = age1 + age2 + age3 + age4 + age5
+
+  age = add_age / 5
+
+  return age
+end
+
+p calculate_bmi(15, 18, 16 , 23, 12)
+
+
